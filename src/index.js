fa269922f9c7f1d9f65e59ecededc62350481a22
Steve Zimmers <steve@soccer-1.com>
2018-01-11 12:07:14 -0500

    ensure router basics are working

--- src/index.js
+++ src/index.js
@@ -1,15 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import {BrowserRouter, Route} from 'react-router-dom';
 
 import App from './components/app';
 import reducers from './reducers';
 
 const createStoreWithMiddleware = applyMiddleware()(createStore);
 
+class Foo extends React.Component {
+	render() {
+		return <div>foo</div>
+	}
+}
+
+class Bar extends React.Component {
+	render() {
+		return <div>bar</div>
+	}
+}
+
 ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	<Provider store={createStoreWithMiddleware(reducers)}>
+		<BrowserRouter>
+			<div>
+				<Route path="/foo" component={Foo}/>
+				<Route path="/bar" component={Bar}/>
+			</div>
+		</BrowserRouter>
+	</Provider>
+	, document.querySelector('.container'));
