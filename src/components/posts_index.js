a73111babe59a5ebb6f3a6e655fe5e5f81d50398
Steve Zimmers <steve@soccer-1.com>
2018-01-12 15:59:16 -0500

    - add nav button - start adding redux-form code

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import _ from 'lodash';
+import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {fetchPosts} from '../actions/index';
 
@@ -23,6 +24,12 @@ class PostsIndex extends Component {
 	render() {
 		return (
 			<div>
+				<div className="text-xs-right">
+					<Link className="btn btn-primary" to="/posts/new">
+						New Post
+					</Link>
+				</div>
+
 				<h3>Posts</h3>
 				<ul className="list-group">
 					{this.renderPosts()}
