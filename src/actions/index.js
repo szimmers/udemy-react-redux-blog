361cc840674559fd1fa984f9726db0f7f48e2f4b
Steve Zimmers <steve@soccer-1.com>
2018-01-12 12:27:35 -0500

    create redux stuff necessary for fetching first posts, then display them

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
+//const API_KEY = '?key=tiruk3j3';
+const API_KEY = '?key=1234';
+
+export const FETCH_POSTS = 'FETCH_POSTS';
+
+export function fetchPosts() {
+	const url = `${ROOT_URL}/posts${API_KEY}`;
+	const request = axios.get(url);
+
+	return {
+		type: FETCH_POSTS,
+		payload: request
+	};
+}
--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,13 +1,44 @@
 import React, {Component} from 'react';
+import _ from 'lodash';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions/index';
 
 class PostsIndex extends Component {
+	componentWillMount() {
+	//componentDidMount() {
+		this.props.fetchPosts();
+	}
+
+	renderPosts() {
+		return _.map(this.props.posts, post => {
+			return (
+				<li className="list-group-item" key={post.id}>
+					<h5>{post.title}</h5>
+					{post.content}
+				</li>
+			);
+		});
+	}
+
 	render() {
 		return (
 			<div>
-				posts index
+				<h3>Posts</h3>
+				<ul className="list-group">
+					{this.renderPosts()}
+				</ul>
 			</div>
 		);
 	}
 }
 
-export default PostsIndex;
+/**
+ * for getting app state into component
+ * @param state
+ * @returns {{posts: *|Function}}
+ */
+function mapStateToProps(state) {
+	return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndex);
