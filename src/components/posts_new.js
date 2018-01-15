a069c76e306abb5e7d3f0da7c6d4b99b0bf6a133
Steve Zimmers <steve@soccer-1.com>
2018-01-15 15:43:14 -0500

    ensure error text shows up only after user interacts w/ field

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -10,7 +10,7 @@ class PostsNew extends Component {
 					   className="form-control"
 					   {...field.input}
 				/>
-				{field.meta.error}
+				{field.meta.touched ? field.meta.error: ''}
 			</div>
 		);
 	}
