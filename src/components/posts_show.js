6d7ad23eb3bc88f959b397cb3e62d2e2b57fe89d
Steve Zimmers <steve@soccer-1.com>
2018-01-16 14:35:01 -0500

    further implement component that shows single post

--- src/components/posts_show.js
+++ src/components/posts_show.js
@@ -1,11 +1,38 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions/index';
 
 class PostsShow extends Component {
+	componentDidMount() {
+		const {id} = this.props.match.params;
+		this.props.fetchPost(id);
+	}
+
 	render() {
+		const {post} = this.props;
+
+		if (!post) {
+			return <div>Loading...</div>;
+		}
+
 		return (
-			<div>show a single post</div>
+			<div>
+				<h3>{post.title}</h3>
+				<h6>Categories: {post.categories}</h6>
+				<p>{post.content}</p>
+			</div>
 		);
 	}
 }
 
-export default PostsShow;
+function mapStateToProps({posts}, ownProps) {
+	console.log(ownProps);
+	const {id} = ownProps.match.params;
+	console.log(posts);
+	console.log(posts[id]);
+	return {
+		post: posts[id]
+	}
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostsShow);
