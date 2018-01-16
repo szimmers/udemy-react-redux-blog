faae13346db35bbff1b05c04ff8f9f3d64adceab
Steve Zimmers <steve@soccer-1.com>
2018-01-16 11:09:03 -0500

    - make action creator for creating new post - call it from new post form - update key to something unique

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const ROOT_URL = 'http://reduxblog.herokuapp.com/api';
-//const API_KEY = '?key=tiruk3j3';
-const API_KEY = '?key=1234';
+const API_KEY = '?key=tiruk3j3';
+//const API_KEY = '?key=1234';
 
 export const FETCH_POSTS = 'FETCH_POSTS';
+export const CREATE_POST = 'CREATE_POST';
 
 export function fetchPosts() {
 	const url = `${ROOT_URL}/posts${API_KEY}`;
@@ -12,6 +13,16 @@ export function fetchPosts() {
 
 	return {
 		type: FETCH_POSTS,
+		payload: request
+	};
+}
+
+export function createPost(values) {
+	const url = `${ROOT_URL}/posts${API_KEY}`;
+	const request = axios.post(url, values);
+
+	return {
+		type: CREATE_POST,
 		payload: request
 	};
 }
--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import {Field, reduxForm} from 'redux-form';
 import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {createPost} from '../actions/index';
 
 class PostsNew extends Component {
 	renderField(field) {
@@ -22,7 +24,7 @@ class PostsNew extends Component {
 	}
 
 	onSubmit(values) {
-		console.log(values);
+		this.props.createPost(values);
 	}
 
 	render() {
@@ -78,4 +80,6 @@ function validate(values) {
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew);
+})(
+	connect(null, {createPost})(PostsNew)
+);
